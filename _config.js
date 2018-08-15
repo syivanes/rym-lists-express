ac458f78e9f4687feffe8cde6c5485de67d53d99
syivanes <sergey.ivanes@gmail.com>
2018-08-15 14:39:39 -0400

    resetting repo

--- _config.js
+++
@@ -1,7 +0,0 @@
-const ids = {
-  github: {
-    clientId: '4b7f68f5ad90f08efdbd',
-    clientSecret: '8a0dc76dbf84c45812dd26e6d83989598f8f13f6',
-    callbackURL: 'http://127.0.0.1:3000/auth/github/callback'
-  }
-}
